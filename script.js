75c004308fa7ae59c0c8a70ef3e883951926d959
Eren Çataltepe <erencataltepe@gmail.com>
2025-09-02 19:02:54 +0200

    Add playRound function to play one round

--- script.js
+++ script.js
@@ -11,8 +11,32 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   const userChoice = prompt("What is your choice?");
-  return userChoice;
+  return userChoice.toLowerCase();
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (computerChoice == "rock" && humanChoice == "rock") {
+    console.log("It's a draw");
+  } else if (computerChoice == "rock" && humanChoice == "paper") {
+    console.log("You win! Paper beats Rock");
+  } else if (computerChoice == "rock" && humanChoice == "scissors") {
+    console.log("You lose! Rock beats scissors.");
+  } else if (computerChoice == "paper" && humanChoice == "paper") {
+    console.log("It's a draw");
+  } else if (computerChoice == "paper" && humanChoice == "scissors") {
+    console.log("You win! Scissors beats paper.");
+  } else if (computerChoice == "paper" && humanChoice == "rock") {
+    console.log("You lose! Paper beats Rock");
+  } else if (computerChoice == "scissors" && humanChoice == "scissors") {
+    console.log("It's a draw");
+  } else if (computerChoice == "scissors" && humanChoice == "rock") {
+    console.log("You win! Scissors beats rock!");
+  } else if (computerChoice == "scissors" && humanChoice == "paper") {
+    console.log("You lose! Scissors beats paper!");
+  } else console.log("Invalid input!");
+}
+
+playRound(getHumanChoice(), getComputerChoice());
